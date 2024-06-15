a8c2f8ebb890cf12084cd9901b792d79199fa946
camizgon <camizgon22@hotmail.com>
2024-06-14 22:08:53 -0400

    Cuarto Commit

--- assets/js/Index.js
+++ assets/js/Index.js
@@ -1,32 +1,28 @@
-// Punto 5: Dividir el código en módulos
-import {
-  Leon,
-  Lobo,
-  Oso,
-  Serpiente,
-  Aguila,
-} from "./Dominio/TiposDeAnimales.js";
+// Importamos las clases de animales desde un módulo
+import { Leon, Lobo, Oso, Serpiente, Aguila } from "./Dominio/TiposDeAnimales.js";
 
-// Función IIFE - Punto 4: Función autoejecutable IIFE
+// Función autoejecutable IIFE - Punto 4: Función autoejecutable IIFE
 (() => {
-  // busca el elemento del doc HTML (btn) este se encuentra esperando el click del usuario para activar el código dentro de la función flecha. Evento de clic en el botón "Registrar"
+ // busca el elemento del doc HTML (btn) este se encuentra esperando el click del usuario para activar el código dentro de la función flecha. Evento de clic en el botón "Registrar"
   document.getElementById("btnRegistrar").addEventListener("click", () => {
+    // Obtener valores del formulario
     const nombre = document.getElementById("animal").value;
     const edad = document.getElementById("edad").value;
     const comentarios = document.getElementById("comentarios").value;
 
-    // Condición que verifica que estas variables no tengan un valor asigando
+    // Condición que valida que todos los campos del formulario estén completos
     if (!nombre || !edad || !comentarios) {
-      // si alguna de estas no tiene un valor asigando se ejecuta el alert
+         // si alguna de estas no tiene un valor asigando se ejecuta el alert
       alert("Por favor, complete todos los campos");
       return;
     }
 
-    // Declaración de variables, que almacenará las rutas de las imagenes y sonido
+    //Declaración de variables, que almacenará las rutas de las imagenes y sonido
     let imgPath, sonido;
     // se crea la varible animal para crear la instancia de un tipo de animal especifico
     let animal;
-    // se ejecuta a base de la variable nombre, corresponde al tipo de animal seleccionado por el usuario, depende de este se asigan las rutas a imagenes y sonidos especificas de cada animal
+
+    // Crear instancia del animal seleccionado por el usuario según el nombre, depende de esto se le asignan las rutas eimagenes especificas de cada animal
     switch (nombre) {
       case "Leon":
         imgPath = "./assets/imgs/Leon.png";
@@ -54,68 +50,60 @@ import {
 
     // Una vez salga del bloque switch llama la función fetchImageAndAddToTable con el objeto animal creado como argumento, esta se encarga de cargar la imagen y añadirla a la tabla
     fetchImageAndAddToTable(animal);
-    // esta función restablece el formulario, limpiando los campos de entrada para que el usuario pueda ingresar otro animal.
+
+    // Esta función limpia los campos de entrada del formulario después de registrar el animal
     resetFormulario();
   });
 
-  // llamamos la función con el objeto ya declarados al terminar bloque switch, esta incluye la URL de la imagen e información del animal.
+  // Función asíncrona con el objeto animal para cargar imagen y agregar animal a la tabla
   async function fetchImageAndAddToTable(animal) {
     try {
-      // Solicita la imagen del animal desde la URL. Fetch de la imagen del animal desde la URL proporcionada en animal.img
+      // Consultar imagen del animal desde la URL.  Fetch de la imagen del animal desde la URL proporcionada en animal.img
       const response = await fetch(animal.img);
-      // Verifica si la respuesta es exitosa
+
+      // Verificar si la respuesta del fetch fue exitosa
       if (!response.ok) throw new Error("La respuesta no fue correcta");
-      // Obtiene el blob de la respuesta
+
+      // Convertir respuesta a blob
       const blob = await response.blob();
+
        // Crea un object URL para la imagen
       const objectURL = URL.createObjectURL(blob);
-      // llama a la función agregarAnimalATabla, pasando el objeto animal y la URL del objeto (objectURL) como argumentos. Esta función es responsable de agregar el animal (junto con su imagen) a la tabla en la interfaz de usuario.
+
+      //  // llama a la función agregarAnimalATabla, pasando el objeto animal y la URL del objeto (objectURL) como argumentos. Esta función es responsable de agregar el animal (junto con su imagen) a la tablaN DEL dom.
       agregarAnimalATabla(animal, objectURL);
     } catch (error) {
-      // ejecuta si ocurre algún error durante el proceso fetch. En caso de error, se imprime un mensaje en la consola indicando que hubo un problema al cargar la imagen
+       // ejecuta si ocurre algún error durante el proceso fetch. En caso de error, se imprime un mensaje en la consola indicando que hubo un problema al cargar la imagen.
       console.error("Hubo un problema al cargar la imagen:", error);
-      alert(
-        "Ocurrió un problema al cargar la imagen del animal. Por favor, intente de nuevo."
-      );
+      alert("Ocurrió un problema al cargar la imagen del animal. Por favor, intente de nuevo.");
     }
   }
 
   // llamamos la función agregarAnimalATabla declarara en el fetch, la cual toma dos argumentos un objeto animal y una URL de objeto (Imagen del animal)
+  // Función para agregar animal a la tabla en el DOM
   function agregarAnimalATabla(animal, objectURL) {
     // Elemento del DOM con el id "Animales". Este es el contenedor donde se van a añadir las tarjetas de los animales.
     const tabla = document.getElementById("Animales");
+
     // Se añade un nuevo bloque de HTML al contenido existente en el elemento "Animales". Este bloque de HTML representa una tarjeta (card) que contiene la información del animal
     tabla.innerHTML += `
-        <div class="card m-2" style="width: 200px; height: 300px;" onclick="mostrarModal('${animal.nombre}', '${animal.edad}', '${objectURL}', '${animal.comentarios}')">
-        <img src="${objectURL}" class="card-img-top my-image" alt="${animal.nombre}">
-        <div class="card-body">
+      <div class="card m-2" style="width: 250px; height: 450px;">
+      <img src="${objectURL}" class="card-img-top my-image" alt="${animal.nombre}">
+      <div class="card-body ">
         <h5 class="card-title">${animal.nombre}</h5>
         <p class="card-text">${animal.edad}</p>
-        </div>
-        </div>
-        `;
+        <p>${animal.comentarios}</p>
+        <button onclick="reproducirSonido('${animal.sonido}')" class="btn btn-primary">Reproducir Sonido</button>
+      </div>
+    </div>
+    `;
   }
 
-  // define una función llamada resetFormulario que no toma ningún argumento.
+  // Función para limpiar el formulario después de registrar un animal
   function resetFormulario() {
-    // selecciona el elemento del formulario con el id y se establece su valor (value) a una cadena vacía (""). Esto borra cualquier selección hecha en el campo de selección de animales.
+    // selecciona el elemento del formulario con el id y se establece su valor (value) a una cadena vacía (""). Esto limpia los campos del formulario
     document.getElementById("animal").value = "";
     document.getElementById("edad").value = "";
     document.getElementById("comentarios").value = "";
   }
-
-
-
-  // define una función llamada mostrarModal que muestra un modal con la información del animal
-  window.mostrarModal = function (nombre, edad, img, comentarios) {
-    // Selecciona el elemento del DOM con la clase "modal-body" donde se va a colocar el contenido del modal
-    const modalBody = document.querySelector(".modal-body");
-    // Define el contenido HTML del modal utilizando template literals (`)
-    modalBody.innerHTML = `
-        <img src="${img}" class="img-fluid" alt="${nombre}">
-        <p>${comentarios}</p>
-      `;
-      // Muestra el modal utilizando jQuery: #exampleModal es el selector del modal en el DOM
-    $("#exampleModal").modal("show");
-  };
 })();
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Desafío de Programación Avanzada en JavaScript
+## Este proyecto corresponde a un desafío práctico donde se implementa una aplicación web para registrar y visualizar información sobre animales en investigación. La aplicación utiliza JavaScript con los siguientes enfoques y tecnologías:
+
+#### * Programación Orientada a Objetos (POO): Se crean clases y se implementa la herencia para representar diferentes tipos de animales.
+#### * Manipulación del DOM: Se utiliza JavaScript para interactuar con el DOM y mostrar dinámicamente los animales registrados en una tabla.
+#### *Funciones Asíncronas (async/await): Se emplea para realizar consultas asíncronas para obtener imágenes de los animales desde URLs externas.
+#### *Módulos de ES6: El código está organizado en módulos separados para una mejor estructura y reutilización del código.
+#### * Funciones Autoejecutables IIFE: Se utiliza una función autoejecutable para encapsular la lógica principal y los eventos del DOM.
+#### * Validación de Formulario: Se implementa una validación básica para asegurar que todos los campos necesarios del formulario estén completos antes de registrar un animal.
+#### * Reproducción de Sonido: Se añade la funcionalidad de reproducir el sonido característico de cada animal desde un botón dentro de su tarjeta correspondiente.
+
+## Funcionalidades Principales
+
+#### Registrar Animal: Completa el formulario con el nombre, edad y comentarios del animal. Selecciona el tipo de animal y haz clic en el botón "Registrar".
+#### Visualización: Los animales registrados se mostrarán dinámicamente en forma de tarjetas en la tabla principal.
+#### Reproducir Sonido: Cada tarjeta de animal tiene un botón para reproducir el sonido característico del animal registrado.
+
+### Estructura del Proyecto
+
+#### assets/: Contiene las carpetas con contenido del proyecto, imagenes de los animales, sonidos, estilos, etc
+#### Dominio/: Contiene las clases de los tipos de animales.
+#### index.html: Página principal de la aplicación.
+#### script.js: Archivo principal de JavaScript que contiene la lógica de la aplicación.
+#### styles.css: Hoja de estilos CSS para el diseño y la apariencia de la aplicación.
+
+## Recursos Adicionales
+
+#### Bootstrap: Utilizado para estilos básicos y componentes de interfaz de usuario.
+#### jQuery: Utilizado para mostrar el modal en la versión original del código.
+
+## Desarrollador
+#### Nombre: Camila Zúñiga González
